7334b9fd69f95c5d5af90ee2b0e4bac635e98aed
Luke Collins <luke.g.collins@gmail.com>
2022-12-08 11:17:07 +1100

    Swagger Ui

--- Aboriginal Art Gallery of Australia/Aboriginal Art Gallery of Australia/Program.cs
+++ Aboriginal Art Gallery of Australia/Aboriginal Art Gallery of Australia/Program.cs
@@ -125,6 +125,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+/*    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });*/
 }
 
 app.UseHttpsRedirection();
