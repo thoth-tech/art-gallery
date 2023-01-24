33b5ac8d3bb88d65a32d5cd220d8e3991139b985
Luke Collins <luke.g.collins@gmail.com>
2023-01-24 12:01:30 +1100

    ArtistADO Tidy Up + Artist Active Record Implemented.

--- Aboriginal Art Gallery of Australia/Aboriginal Art Gallery of Australia/Models/Database Models/Artist.cs
+++ Aboriginal Art Gallery of Australia/Aboriginal Art Gallery of Australia/Models/Database Models/Artist.cs
@@ -1,14 +1,12 @@
 ﻿using Aboriginal_Art_Gallery_of_Australia.Models.DTOs;
-using static Aboriginal_Art_Gallery_of_Australia.Persistence.ExtensionMethods;
 using Aboriginal_Art_Gallery_of_Australia.Persistence.Interfaces;
-using Microsoft.Extensions.Configuration;
 using Npgsql;
-using Aboriginal_Art_Gallery_of_Australia.Persistence.Implementations.ADO;
+using static Aboriginal_Art_Gallery_of_Australia.Persistence.ExtensionMethods;
 
 namespace Aboriginal_Art_Gallery_of_Australia.Models.Database_Models
 {
     /// <summary>
-    /// The Artist class is responsible for handling the database model associated with artists.
+    /// The Artist class is responsible for handling the database model associated with artists. The Second half of the Artist class from line 37 down is the implementation of the Active Record design pattern. This is a duplication of code from the ADO implementation for demonstration purposes.
     /// </summary>
     public class Artist : IArtistDataAccess
     {
@@ -37,34 +35,141 @@ namespace Aboriginal_Art_Gallery_of_Australia.Models.Database_Models
             CreatedAt = createdAt;
         }
 
-        // Active Record - Everything under line 39 is required for the active record implementation.
+        // Active Record - Everything under line 37 is required for the active record implementation.
+
         private static string _connectionString = "Host=localhost;Database=Deakin University | AAGoA;Username=postgres;Password=postgreSQL;";
 
         public Artist() { }
 
         public List<ArtistOutputDto> GetArtists()
         {
-            throw new NotImplementedException();
+            List<ArtistOutputDto> artists = new();
+            using NpgsqlConnection connection = new(_connectionString);
+            {
+                connection.Open();
+                using NpgsqlCommand cmd = new("SELECT * FROM artist", connection);
+                {
+                    using NpgsqlDataReader dr = cmd.ExecuteReader();
+                    {
+                        if (dr != null)
+                        {
+                            while (dr.Read())
+                            {
+                                int artistId = (int)dr["artist_id"];
+                                string firstName = (string)dr["first_name"];
+                                string lastName = (string)dr["last_name"];
+                                string displayName = (string)dr["display_name"];
+                                string profileImageURL = (string)dr["profile_image_url"];
+                                string placeOfBirth = (string)dr["place_of_birth"];
+                                int yearOfBirth = (int)dr["year_of_birth"];
+                                int? yearOfDeath = ConvertFromNullableValue<int?>(dr["year_of_death"]);
+                                DateTime modifiedAt = (DateTime)dr["modified_at"];
+                                DateTime createdAt = (DateTime)dr["created_at"];
+                                artists.Add(new ArtistOutputDto(artistId, firstName, lastName, displayName, profileImageURL, placeOfBirth, yearOfBirth, yearOfDeath, modifiedAt, createdAt));
+                            }
+                        }
+                        return artists;
+                    }
+                }
+            }
         }
 
         public ArtistOutputDto? GetArtistById(int id)
         {
-            throw new NotImplementedException();
+            using NpgsqlConnection connection = new(_connectionString);
+            {
+                connection.Open();
+                using NpgsqlCommand cmd = new("SELECT * FROM artist WHERE artist_id = @artistId", connection);
+                {
+                    cmd.Parameters.AddWithValue("@artistId", id);
+                    using NpgsqlDataReader dr = cmd.ExecuteReader();
+                    {
+                        if (dr != null)
+                        {
+                            while (dr.Read())
+                            {
+                                int artistId = (int)dr["artist_id"];
+                                string firstName = (string)dr["first_name"];
+                                string lastName = (string)dr["last_name"];
+                                string displayName = (string)dr["display_name"];
+                                string profileImageURL = (string)dr["profile_image_url"];
+                                string placeOfBirth = (string)dr["place_of_birth"];
+                                int yearOfBirth = (int)dr["year_of_birth"];
+                                int? yearOfDeath = ConvertFromNullableValue<int?>(dr["year_of_death"]);
+                                DateTime modifiedAt = (DateTime)dr["modified_at"];
+                                DateTime createdAt = (DateTime)dr["created_at"];
+                                return new ArtistOutputDto(artistId, firstName, lastName, displayName, profileImageURL, placeOfBirth, yearOfBirth, yearOfDeath, modifiedAt, createdAt);
+                            }
+                        }
+                        return null;
+                    }
+                }
+            }
         }
 
         public ArtistInputDto? InsertArtist(ArtistInputDto artist)
         {
-            throw new NotImplementedException();
+            using NpgsqlConnection connection = new(_connectionString);
+            {
+                connection.Open();
+                using NpgsqlCommand cmd = new("INSERT INTO artist(first_name, last_name, display_name, profile_image_url,place_of_birth, year_of_birth, year_of_death, modified_at, created_at) " +
+                                              "VALUES (@firstName, @lastName, @displayName, @profileImageURL, @placeOfBirth, @yearOfBirth, @yearOfDeath, current_timestamp, current_timestamp);", connection);
+                {
+                    cmd.Parameters.AddWithValue("@firstName", artist.FirstName);
+                    cmd.Parameters.AddWithValue("@lastName", artist.LastName);
+                    cmd.Parameters.AddWithValue("@displayName", artist.DisplayName);
+                    cmd.Parameters.AddWithValue("@profileImageURL", artist.ProfileImageUrl);
+                    cmd.Parameters.AddWithValue("@placeOfBirth", artist.PlaceOfBirth);
+                    cmd.Parameters.AddWithNullableValue("@yearOfBirth", artist.YearOfBirth);
+                    cmd.Parameters.AddWithNullableValue("@yearOfDeath", artist.YearOfDeath);
+                    int result = cmd.ExecuteNonQuery();
+                    return result is 1 ? artist : null;
+                }
+            }
         }
 
         public ArtistInputDto? UpdateArtist(int id, ArtistInputDto artist)
         {
-            throw new NotImplementedException();
+            using NpgsqlConnection connection = new(_connectionString);
+            {
+                connection.Open();
+                using NpgsqlCommand cmd = new("UPDATE artist " +
+                                              "SET first_name = @firstName, " +
+                                                "last_name = @lastName, " +
+                                                "display_name = @displayName, " +
+                                                "profile_image_url = @profileImageURL, " +
+                                                "place_of_birth = @placeOfBirth, " +
+                                                "year_of_birth = @yearOfBirth, " +
+                                                "year_of_death = @yearOfDeath, " +
+                                                "modified_at = current_timestamp " +
+                                              "WHERE artist_id = @artistId", connection);
+                {
+                    cmd.Parameters.AddWithValue("@artistId", id);
+                    cmd.Parameters.AddWithValue("@firstName", artist.FirstName);
+                    cmd.Parameters.AddWithValue("@lastName", artist.LastName);
+                    cmd.Parameters.AddWithValue("@displayName", artist.DisplayName);
+                    cmd.Parameters.AddWithValue("@profileImageURL", artist.ProfileImageUrl);
+                    cmd.Parameters.AddWithValue("@placeOfBirth", artist.PlaceOfBirth);
+                    cmd.Parameters.AddWithNullableValue("@yearOfBirth", artist.YearOfBirth);
+                    cmd.Parameters.AddWithNullableValue("@yearOfDeath", artist.YearOfDeath);
+                    int result = cmd.ExecuteNonQuery();
+                    return result is 1 ? artist : null;
+                }
+            }
         }
 
         public bool DeleteArtist(int id)
         {
-            throw new NotImplementedException();
+            using NpgsqlConnection connection = new(_connectionString);
+            {
+                connection.Open();
+                using NpgsqlCommand cmd = new("DELETE FROM artist WHERE artist_id = @artistId", connection);
+                {
+                    cmd.Parameters.AddWithValue("@artistId", id);
+                    int result = cmd.ExecuteNonQuery();
+                    return result == 1;
+                }
+            }
         }
     }
 }
--- Aboriginal Art Gallery of Australia/Aboriginal Art Gallery of Australia/Persistence/Implementations/ADO/ArtistADO.cs
+++ Aboriginal Art Gallery of Australia/Aboriginal Art Gallery of Australia/Persistence/Implementations/ADO/ArtistADO.cs
@@ -17,28 +17,28 @@ namespace Aboriginal_Art_Gallery_of_Australia.Persistence.Implementations.ADO
 
         public List<ArtistOutputDto> GetArtists()
         {
-            var artists = new List<ArtistOutputDto>();
-            using var connection = new NpgsqlConnection(_configuration.GetConnectionString("PostgresSQL"));
+            List<ArtistOutputDto> artists = new();
+            using NpgsqlConnection connection = new(_configuration.GetConnectionString("PostgresSQL"));
             {
                 connection.Open();
-                using var cmd = new NpgsqlCommand("SELECT * FROM artist", connection);
+                using NpgsqlCommand cmd = new("SELECT * FROM artist", connection);
                 {
-                    using var dr = cmd.ExecuteReader();
+                    using NpgsqlDataReader dr = cmd.ExecuteReader();
                     {
                         if (dr != null)
                         {
                             while (dr.Read())
                             {
-                                var artistId = (int)dr["artist_id"];
-                                var firstName = (string)dr["first_name"];
-                                var lastName = (string)dr["last_name"];
-                                var displayName = (string)dr["display_name"];
-                                var profileImageURL = (string)dr["profile_image_url"];
-                                var placeOfBirth = (string)dr["place_of_birth"];
-                                var yearOfBirth = (int)dr["year_of_birth"];
-                                var yearOfDeath = ConvertFromNullableValue<int?>(dr["year_of_death"]);
-                                var modifiedAt = (DateTime)dr["modified_at"];
-                                var createdAt = (DateTime)dr["created_at"];
+                                int artistId = (int)dr["artist_id"];
+                                string firstName = (string)dr["first_name"];
+                                string lastName = (string)dr["last_name"];
+                                string displayName = (string)dr["display_name"];
+                                string profileImageURL = (string)dr["profile_image_url"];
+                                string placeOfBirth = (string)dr["place_of_birth"];
+                                int yearOfBirth = (int)dr["year_of_birth"];
+                                int? yearOfDeath = ConvertFromNullableValue<int?>(dr["year_of_death"]);
+                                DateTime modifiedAt = (DateTime)dr["modified_at"];
+                                DateTime createdAt = (DateTime)dr["created_at"];
                                 artists.Add(new ArtistOutputDto(artistId, firstName, lastName, displayName, profileImageURL, placeOfBirth, yearOfBirth, yearOfDeath, modifiedAt, createdAt));
                             }
                         }
@@ -50,45 +50,44 @@ namespace Aboriginal_Art_Gallery_of_Australia.Persistence.Implementations.ADO
 
         public ArtistOutputDto? GetArtistById(int id)
         {
-            using var connection = new NpgsqlConnection(_configuration.GetConnectionString("PostgresSQL"));
+            using NpgsqlConnection connection = new(_configuration.GetConnectionString("PostgresSQL"));
             {
                 connection.Open();
-                using var cmd = new NpgsqlCommand("SELECT * FROM artist WHERE artist_id = @artistId", connection);
+                using NpgsqlCommand cmd = new("SELECT * FROM artist WHERE artist_id = @artistId", connection);
                 {
                     cmd.Parameters.AddWithValue("@artistId", id);
-                    using var dr = cmd.ExecuteReader();
+                    using NpgsqlDataReader dr = cmd.ExecuteReader();
                     {
                         if (dr != null)
                         {
                             while (dr.Read())
                             {
-                                var artistId = (int)dr["artist_id"];
-                                var firstName = (string)dr["first_name"];
-                                var lastName = (string)dr["last_name"];
-                                var displayName = (string)dr["display_name"];
-                                var profileImageURL = (string)dr["profile_image_url"];
-                                var placeOfBirth = (string)dr["place_of_birth"];
-                                var yearOfBirth = (int)dr["year_of_birth"];
-                                var yearOfDeath = ConvertFromNullableValue<int?>(dr["year_of_death"]);
-                                var modifiedAt = (DateTime)dr["modified_at"];
-                                var createdAt = (DateTime)dr["created_at"];
+                                int artistId = (int)dr["artist_id"];
+                                string firstName = (string)dr["first_name"];
+                                string lastName = (string)dr["last_name"];
+                                string displayName = (string)dr["display_name"];
+                                string profileImageURL = (string)dr["profile_image_url"];
+                                string placeOfBirth = (string)dr["place_of_birth"];
+                                int yearOfBirth = (int)dr["year_of_birth"];
+                                int? yearOfDeath = ConvertFromNullableValue<int?>(dr["year_of_death"]);
+                                DateTime modifiedAt = (DateTime)dr["modified_at"];
+                                DateTime createdAt = (DateTime)dr["created_at"];
                                 return new ArtistOutputDto(artistId, firstName, lastName, displayName, profileImageURL, placeOfBirth, yearOfBirth, yearOfDeath, modifiedAt, createdAt);
                             }
                         }
                         return null;
                     }
                 }
-
             }
         }
 
         public ArtistInputDto? InsertArtist(ArtistInputDto artist)
         {
-            using var connection = new NpgsqlConnection(_configuration.GetConnectionString("PostgresSQL"));
+            using NpgsqlConnection connection = new(_configuration.GetConnectionString("PostgresSQL"));
             {
                 connection.Open();
-                using var cmd = new NpgsqlCommand("INSERT INTO artist(first_name, last_name, display_name, profile_image_url,place_of_birth, year_of_birth, year_of_death, modified_at, created_at) " +
-                                                  "VALUES (@firstName, @lastName, @displayName, @profileImageURL, @placeOfBirth, @yearOfBirth, @yearOfDeath, current_timestamp, current_timestamp);", connection);
+                using NpgsqlCommand cmd = new("INSERT INTO artist(first_name, last_name, display_name, profile_image_url,place_of_birth, year_of_birth, year_of_death, modified_at, created_at) " +
+                                              "VALUES (@firstName, @lastName, @displayName, @profileImageURL, @placeOfBirth, @yearOfBirth, @yearOfDeath, current_timestamp, current_timestamp);", connection);
                 {
                     cmd.Parameters.AddWithValue("@firstName", artist.FirstName);
                     cmd.Parameters.AddWithValue("@lastName", artist.LastName);
@@ -97,45 +96,77 @@ namespace Aboriginal_Art_Gallery_of_Australia.Persistence.Implementations.ADO
                     cmd.Parameters.AddWithValue("@placeOfBirth", artist.PlaceOfBirth);
                     cmd.Parameters.AddWithNullableValue("@yearOfBirth", artist.YearOfBirth);
                     cmd.Parameters.AddWithNullableValue("@yearOfDeath", artist.YearOfDeath);
-                    var result = cmd.ExecuteNonQuery();
+                    int result = cmd.ExecuteNonQuery();
                     return result is 1 ? artist : null;
                 }
             }
         }
 
+        // Option 1: The update function requires all DTO fields to be complete. Meaning database records can not be partially updated.
         public ArtistInputDto? UpdateArtist(int id, ArtistInputDto artist)
         {
-            using var connection = new NpgsqlConnection(_configuration.GetConnectionString("PostgresSQL"));
+            using NpgsqlConnection connection = new(_configuration.GetConnectionString("PostgresSQL"));
+            {
+                connection.Open();
+                using NpgsqlCommand cmd = new("UPDATE artist " +
+                                              "SET first_name = @firstName, " +
+                                                "last_name = @lastName, " +
+                                                "display_name = @displayName, " +
+                                                "profile_image_url = @profileImageURL, " +
+                                                "place_of_birth = @placeOfBirth, " +
+                                                "year_of_birth = @yearOfBirth, " +
+                                                "year_of_death = @yearOfDeath, " +
+                                                "modified_at = current_timestamp " +
+                                              "WHERE artist_id = @artistId", connection);
+                {
+                    cmd.Parameters.AddWithValue("@artistId", id);
+                    cmd.Parameters.AddWithValue("@firstName", artist.FirstName);
+                    cmd.Parameters.AddWithValue("@lastName", artist.LastName);
+                    cmd.Parameters.AddWithValue("@displayName", artist.DisplayName);
+                    cmd.Parameters.AddWithValue("@profileImageURL", artist.ProfileImageUrl);
+                    cmd.Parameters.AddWithValue("@placeOfBirth", artist.PlaceOfBirth);
+                    cmd.Parameters.AddWithNullableValue("@yearOfBirth", artist.YearOfBirth);
+                    cmd.Parameters.AddWithNullableValue("@yearOfDeath", artist.YearOfDeath);
+                    int result = cmd.ExecuteNonQuery();
+                    return result is 1 ? artist : null;
+                }
+            }
+        }
+
+        // Option 2: This update function does not require all DTO fields to be complete. Any field left null, “”, or its default value; i.e., string, 0, ect, will not be updated. Nullable fields can not be set back to null after being updated.
+        /*public ArtistInputDto? UpdateArtist(int id, ArtistInputDto artist)
+        {
+            using NpgsqlConnection connection = new(_configuration.GetConnectionString("PostgresSQL"));
             {
                 connection.Open();
 
-                String cmdString = "UPDATE artist SET ";
+                string cmdString = "UPDATE artist SET ";
 
-                if (artist.FirstName is not null && artist.FirstName != "" && artist.FirstName != "string")
+                if (artist.FirstName is not null and not "" and not "string")
                 {
                     cmdString += "first_name = @firstName, ";
                 }
-                if (artist.LastName is not null && artist.LastName != "" && artist.LastName != "string")
+                if (artist.LastName is not null and not "" and not "string")
                 {
                     cmdString += "last_name = @lastName, ";
                 }
-                if (artist.DisplayName is not null && artist.DisplayName != "" && artist.DisplayName != "string")
+                if (artist.DisplayName is not null and not "" and not "string")
                 {
                     cmdString += "display_name = @displayName, ";
                 }
-                if (artist.ProfileImageUrl is not null && artist.ProfileImageUrl != "" && artist.ProfileImageUrl != "string")
+                if (artist.ProfileImageUrl is not null and not "" and not "string")
                 {
                     cmdString += "profile_image_url = @profileImageURL, ";
                 }
-                if (artist.PlaceOfBirth is not null && artist.PlaceOfBirth != "" && artist.PlaceOfBirth != "string")
+                if (artist.PlaceOfBirth is not null and not "" and not "string")
                 {
                     cmdString += "place_of_birth = @placeOfBirth, ";
                 }
-                if (artist.YearOfBirth is not null && artist.YearOfBirth != 0)
+                if (artist.YearOfBirth is not null and not 0)
                 {
                     cmdString += "year_of_birth = @yearOfBirth, ";
                 }
-                if (artist.YearOfDeath is not null && artist.YearOfDeath != 0)
+                if (artist.YearOfDeath is not null and not 0)
                 {
                     cmdString += "year_of_death = @yearOfDeath, ";
                 }
@@ -143,52 +174,52 @@ namespace Aboriginal_Art_Gallery_of_Australia.Persistence.Implementations.ADO
                 cmdString += "modified_at = current_timestamp WHERE artist_id = @artistId";
 
 
-                using var cmd = new NpgsqlCommand(cmdString, connection);
+                using NpgsqlCommand cmd = new(cmdString, connection);
                 {
                     cmd.Parameters.AddWithValue("@artistId", id);
-                    if (artist.FirstName is not null && artist.FirstName != "" && artist.FirstName != "string")
+                    if (artist.FirstName is not null and not "" and not "string")
                     {
                         cmd.Parameters.AddWithValue("@firstName", artist.FirstName);
                     }
-                    if (artist.LastName is not null && artist.LastName != "" && artist.LastName != "string")
+                    if (artist.LastName is not null and not "" and not "string")
                     {
                         cmd.Parameters.AddWithValue("@lastName", artist.LastName);
                     }
-                    if (artist.DisplayName is not null && artist.DisplayName != "" && artist.DisplayName != "string")
+                    if (artist.DisplayName is not null and not "" and not "string")
                     {
                         cmd.Parameters.AddWithValue("@displayName", artist.DisplayName);
                     }
-                    if (artist.ProfileImageUrl is not null && artist.ProfileImageUrl != "" && artist.ProfileImageUrl != "string")
+                    if (artist.ProfileImageUrl is not null and not "" and not "string")
                     {
                         cmd.Parameters.AddWithValue("@profileImageURL", artist.ProfileImageUrl);
                     }
-                    if (artist.PlaceOfBirth is not null && artist.PlaceOfBirth != "" && artist.PlaceOfBirth != "string")
+                    if (artist.PlaceOfBirth is not null and not "" and not "string")
                     {
                         cmd.Parameters.AddWithValue("@placeOfBirth", artist.PlaceOfBirth);
                     }
-                    if (artist.YearOfBirth is not null && artist.YearOfBirth != 0)
+                    if (artist.YearOfBirth is not null and not 0)
                     {
                         cmd.Parameters.AddWithNullableValue("@yearOfBirth", artist.YearOfBirth);
                     }
-                    if (artist.YearOfDeath is not null && artist.YearOfDeath != 0)
+                    if (artist.YearOfDeath is not null and not 0)
                     {
                         cmd.Parameters.AddWithNullableValue("@yearOfDeath", artist.YearOfDeath);
                     }
-                    var result = cmd.ExecuteNonQuery();
+                    int result = cmd.ExecuteNonQuery();
                     return result is 1 ? artist : null;
                 }
             }
-        }
+        }*/
 
         public bool DeleteArtist(int id)
         {
-            using var connection = new NpgsqlConnection(_configuration.GetConnectionString("PostgresSQL"));
+            using NpgsqlConnection connection = new(_configuration.GetConnectionString("PostgresSQL"));
             {
                 connection.Open();
-                using var cmd = new NpgsqlCommand("DELETE FROM artist WHERE artist_id = @artistId", connection);
+                using NpgsqlCommand cmd = new("DELETE FROM artist WHERE artist_id = @artistId", connection);
                 {
                     cmd.Parameters.AddWithValue("@artistId", id);
-                    var result = cmd.ExecuteNonQuery();
+                    int result = cmd.ExecuteNonQuery();
                     return result == 1;
                 }
             }
