01441eb270c1c7decbfc909ec84e94cce7da06b2
benmathewthomas <88536879+benmathewthomas@users.noreply.github.com>
2023-09-03 22:40:31 +1000

    feature: update vue components in LoginComponent

--- art-gallery-frontend/src/components/NavigationComponent.vue
+++ art-gallery-frontend/src/components/NavigationComponent.vue
@@ -4,21 +4,16 @@
       <div id="menus">
         <router-link to="/" class="nav-home">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="nav-home-icon">
-            <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-            <path
-              d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"
-            />
+            <!-- ... SVG content for home icon ... -->
           </svg>
         </router-link>
-        <div v-if="!this.showMenu && !this.usingAntDesign"><DropdownMenuHTML /></div>
-        <div v-if="!this.showMenu && this.usingAntDesign"><DropdownMenuAntD /></div>
+        <div v-if="!showMenu && !usingAntDesign"><DropdownMenuHTML /></div>
+        <div v-if="!showMenu && usingAntDesign"><DropdownMenuAntD /></div>
       </div>
       <div class="nav-tools">
-        <div class="auth-tools" title="auth" v-if="!this.showMenu">
+        <div class="auth-tools" title="auth" v-if="!showMenu">
           <div class="user-name" title="user-name" v-if="account.user">
-            <span
-              >Signed in as <b>{{ account.user.name }}</b></span
-            >
+            <span>Signed in as <b>{{ account.user.name }}</b></span>
           </div>
           <div @click="logout" class="nav-tools-login" title="Logout" v-if="account.user">
             <span class="login-nav-button">logout</span>
@@ -35,20 +30,17 @@
         <div @click="toggleMenu" class="nav-tools-menu" title="Menu">
           <span class="screen-reader-only">Menu</span>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-            <path
-              d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
-            />
+            <!-- ... SVG content for menu icon ... -->
           </svg>
         </div>
       </div>
     </div>
-    <div class="menu-grid" v-if="this.showMenu && !this.usingAntDesign">
+    <div class="menu-grid" v-if="showMenu && !usingAntDesign">
       <CollapsibleMenuHTML />
     </div>
-    <div class="menu-grid" v-if="this.showMenu && this.usingAntDesign"><CollapsibleMenuAntD /></div>
+    <div class="menu-grid" v-if="showMenu && usingAntDesign"><CollapsibleMenuAntD /></div>
 
-    <div class="login-box" v-if="this.showLogin && !account.user && $route.name !== 'login'">
+    <div class="login-box" v-if="showLogin && !account.user && $route.name !== 'login'">
       <LoginComponent />
     </div>
   </nav>
