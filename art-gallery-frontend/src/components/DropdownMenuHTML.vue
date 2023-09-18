3efaf4eef7de15d9e27d58474b0eb4c9e140b3ec
Avenue <ajoju@deakin.edu.au>
2023-09-18 22:21:21 +1000

    feature: Updated DropDownHTML.vue by modfiying the content

--- art-gallery-frontend/src/components/DropdownMenuHTML.vue
+++ art-gallery-frontend/src/components/DropdownMenuHTML.vue
@@ -1,111 +1,107 @@
 <template>
-  <div class="nav-site">
-    <ul class="nav-menu">
-      <li>
-        <router-link to="/" class="nav-link">
-          <span>Home</span>
-        </router-link>
-      </li>
-      <li>
-        <router-link to="/artworks" class="nav-link subnav">
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-          />
-          <span>Artworks <i class="fa fa-caret-down"></i></span>
-          <div class="subnav-content">
-            <router-link to="/artworkofday" class="nav-sub">
-              <span>Artwork of the Day</span>
-            </router-link>
-            <br />
-            <br />
-            <router-link to="/artworks" class="nav-sub">
-              <span>List of Artworks</span>
-            </router-link>
-          </div>
-        </router-link>
-      </li>
-      <li>
-        <router-link to="/culture" class="nav-link subnav">
-          <span>Art & Culture <i class="fa fa-caret-down"></i></span>
-          <div class="subnav-content">
-            <span class="nav-sub">Symbols</span>
-            <br />
-            <br />
-            <router-link to="/artistofday" class="nav-sub">
-              <span>Artist of the Day</span>
-            </router-link>
-            <br />
-            <br />
-            <span class="nav-sub">Aboriginal Art Facts</span>
-          </div>
-        </router-link>
-      </li>
-      <li>
-        <router-link to="/exhibition" class="nav-link">
-          <span>Exhibitions</span>
-        </router-link>
-      </li>
-    </ul>
-    <div v-if="isAdmin()">
+    <div class="nav-site">
       <ul class="nav-menu">
+        <li>
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="subnav">
+          <router-link to="/artworks" class="nav-link">
+            <i class="fa fa-caret-down"></i>
+            Artworks
+          </router-link>
+          <ul class="subnav-content">
+            <li>
+              <router-link to="/artworkofday" class="nav-sub">Artwork of the Day</router-link>
+            </li>
+            <br />
+            <br />
+            <li>
+              <router-link to="/artworks" class="nav-sub">List of Artworks</router-link>
+            </li>
+          </ul>
+        </li>
+        <li class="subnav">
+          <router-link to="/culture" class="nav-link">
+            <i class="fa fa-caret-down"></i>
+            Art & Culture
+          </router-link>
+          <ul class="subnav-content">
+            <li>
+              <span class="nav-sub">Symbols</span>
+            </li>
+            <br />
+            <br />
+            <li>
+              <router-link to="/artistofday" class="nav-sub">Artist of the Day</router-link>
+            </li>
+            <br />
+            <br />
+            <li>
+              <span class="nav-sub">Aboriginal Art Facts</span>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <router-link to="/exhibition" class="nav-link">Exhibitions</router-link>
+        </li>
+      </ul>
+      <ul class="nav-menu" v-if="isAdmin()">
         <li>
           <a href="https://localhost:7194/swagger/index.html" class="nav-link">Swagger</a>
         </li>
       </ul>
     </div>
-  </div>
-</template>
-
-<script>
-import { mapState } from "vuex";
-
-export default {
-  name: "DropdownMenuHTML",
-  methods: {
-    isAdmin() {
-      if (this.account.user) {
-        return this.account.user.role == "Admin";
-      }
+  </template>
+  
+  <script>
+  import { mapState } from "vuex";
+  
+  export default {
+    name: "DropdownMenu",
+    computed: {
+      ...mapState({
+        account: (state) => state.account,
+      }),
     },
-  },
-  computed: {
-    ...mapState({
-      account: (state) => state.account,
-    }),
-  },
-};
-</script>
-
-<style scoped>
-.subnav {
-  overflow: hidden;
-}
-
-.subnav-content {
-  display: none;
-  position: absolute;
-  z-index: 1;
-  background-color: white;
-  font-size: smaller;
-  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.205);
-}
-
-.subnav:hover .subnav-content {
-  display: block;
-  margin-left: 15px;
-  padding: 10px;
-  color: black;
-}
-
-.nav-sub {
-  font-size: 16px;
-  padding-left: 0;
-}
-.nav-sub:hover {
-  color: var(--color--turquoise-light-hover);
-  transition: 0.1s;
-  transition-delay: 0;
-  border-bottom: 2px solid;
-}
-</style>
+    methods: {
+      isAdmin() {
+        return this.account?.user?.role === "Admin";
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .subnav {
+    position: relative;
+  }
+  
+  .subnav-content {
+    display: none;
+    position: absolute;
+    z-index: 1;
+    background-color: white;
+    font-size: smaller;
+    box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.205);
+  }
+  
+  .subnav:hover .subnav-content {
+    display: block;
+    margin-left: 15px;
+    padding: 10px;
+    color: black;
+  }
+  
+  .nav-sub {
+    font-size: 16px;
+    padding-left: 0;
+  }
+  
+  .nav-sub:hover {
+    color: var(--color--turquoise-light-hover);
+    transition: 0.1s;
+    transition-delay: 0;
+    border-bottom: 2px solid;
+  }
+  </style>
+  
